c7271215199c84d03f475f1be587a0fdc5ee76cd
Gopal Menon <gopal.mnn@gmail.com>
2017-02-18 21:16:26 -0700

    Changed weather lookup to return weather condition as a row with all zeroes except with 1 in the column corresponding to the weather condition found.

--- WeatherLookup.py
+++ WeatherLookup.py
@@ -67,6 +67,8 @@ class WeatherLookup(object):
         # Create storage to hold weather data
         self.data_set = data_set
         self.weather_data = None
+        self.weather_data_keys = None
+        self.all_weather_conditions = list()
 
         # Initialize files list based on which environment is being worked on
         data_files_path = None
@@ -90,11 +92,53 @@ class WeatherLookup(object):
 
                 record_tokens = record.split(FileIo.TAB_CHARACTER)
                 unsorted_weather_data[record_tokens[0]] \
-                    = WeatherSnapshot(record_tokens[1], record_tokens[2], record_tokens[3].strip())
+                    = WeatherSnapshot(int(record_tokens[1]), float(record_tokens[2]), float(record_tokens[3].strip()))
 
         # Sort the weather data so that searching on timestamp is possible
         self.weather_data = OrderedDict(sorted(unsorted_weather_data.items(),
                                             key = lambda x: time.mktime(time.strptime(x[0], FileIo.TIMESTAMP_FORMAT))))
+        self.weather_data_keys = list(self.weather_data.keys())
+
+        self.__store_all_weather_conditions()
+
+    """
+    Go through train and test to find all possible weather conditions
+    """
+    def __store_all_weather_conditions(self):
+
+        data_files_path = None
+        data_files_list = None
+        weather_conditions_set = set()
+        for data_set in [FileIo.TRAINING_DATA_SET, FileIo.TEST_DATA_SET]:
+
+            if data_set == FileIo.TRAINING_DATA_SET:
+                data_files_path = WeatherLookup.TRAINING_DATA_WEATHER_FILES_PATH
+                data_files_list = WeatherLookup.TRAINING_DATA_WEATHER_FILES
+            else:
+                data_files_path = WeatherLookup.TESTING_DATA_WEATHER_FILES_PATH
+                data_files_list = WeatherLookup.TESTING_DATA_WEATHER_FILES
+
+            for file_name in data_files_list:
+
+                # Read the file
+                text_file_contents = FileIo.get_text_file_contents(data_files_path + file_name)
+
+                # Loop through the records and load the dictionary lookup
+                for record in text_file_contents:
+                    record_tokens = record.split(FileIo.TAB_CHARACTER)
+                    weather_conditions_set.add(int(record_tokens[1]))
+
+        self.all_weather_conditions = list(weather_conditions_set)
+        self.all_weather_conditions.sort()
+
+    """
+    Return weather condition row with all zeroes except for in the column corresponding to the weather condition found
+    """
+    def __get_weather_condition_row(self, weather_condition):
+
+        weather_condition_row = [0] * len(self.all_weather_conditions)
+        weather_condition_row[self.all_weather_conditions.index(weather_condition)] = 1
+        return weather_condition_row
 
     """
     Return weather snapshot closest to the timestamp parameter
@@ -102,19 +146,18 @@ class WeatherLookup(object):
     def get_weather_snapshot(self, time_stamp):
 
         # find the closest timestamps before and after the input parameter
-        time_stamp_keys = self.weather_data.keys()
-        time_stamp_before_index = bisect.bisect_left(list(time_stamp_keys), time_stamp)
-        time_stamp_after_index = bisect.bisect_right(list(time_stamp_keys), time_stamp)
+        time_stamp_before_index = bisect.bisect_left(self.weather_data_keys, time_stamp)
+        time_stamp_after_index = bisect.bisect_right(self.weather_data_keys, time_stamp)
         if time_stamp_before_index == time_stamp_after_index and time_stamp_before_index != 0:
             time_stamp_before_index -= 1
 
         # Convert timestamps to date time values for easy comparison
-        if time_stamp_before_index >= len(time_stamp_keys):
-            time_stamp_before_index = len(time_stamp_keys) -1
-        if time_stamp_after_index >= len(time_stamp_keys):
-            time_stamp_after_index = len(time_stamp_keys) -1
-        date_time_after = datetime.strptime(list(time_stamp_keys)[time_stamp_after_index], FileIo.TIMESTAMP_FORMAT)
-        date_time_before = datetime.strptime(list(time_stamp_keys)[time_stamp_before_index], FileIo.TIMESTAMP_FORMAT)
+        if time_stamp_before_index >= len(self.weather_data_keys):
+            time_stamp_before_index = len(self.weather_data_keys) -1
+        if time_stamp_after_index >= len(self.weather_data_keys):
+            time_stamp_after_index = len(self.weather_data_keys) -1
+        date_time_after = datetime.strptime(self.weather_data_keys[time_stamp_after_index], FileIo.TIMESTAMP_FORMAT)
+        date_time_before = datetime.strptime(self.weather_data_keys[time_stamp_before_index], FileIo.TIMESTAMP_FORMAT)
         date_time_input = datetime.strptime(time_stamp, FileIo.TIMESTAMP_FORMAT)
 
         # Find out which one is closest to the input parameter
@@ -127,7 +170,16 @@ class WeatherLookup(object):
             closest_index = time_stamp_before_index
 
         # Return the weather corresponding to the entry with timestamp closest to the input timestamp parameter
-        return self.weather_data[list(time_stamp_keys)[closest_index]]
+        weather_snapshot = self.weather_data[self.weather_data_keys[closest_index]]
+
+        # First part of the return value is the weather condition row
+        weather_snapshot_list = self.__get_weather_condition_row(weather_snapshot.get_weather_conditions())
+
+        # Append the temperature and particulate matter
+        weather_snapshot_list.append(weather_snapshot.get_temperature_in_celsius())
+        weather_snapshot_list.append(weather_snapshot.get_particulate_matter())
+
+        return weather_snapshot_list
 
 
 ########################################################################################################################
@@ -142,44 +194,32 @@ class TestWeatherLookup(unittest.TestCase):
     def test_before_range(self):
         weather_lookup = WeatherLookup("test")
         weather = weather_lookup.get_weather_snapshot("2016-01-15 06:12:22")
-        self.assertEquals(weather.get_weather_conditions(), 2) and \
-            self.assertEquals(weather.get_temperature_in_celsius(), 2.0) and \
-            self.assertEquals(weather.get_particulate_matter(), 51)
+        self.assertEquals(weather, [0, 1, 0, 0, 0, 0, 0, 2.0, 51])
 
     def test_start_of_range(self):
         weather_lookup = WeatherLookup("test")
         weather = weather_lookup.get_weather_snapshot("2016-01-22 07:02:10")
-        self.assertEquals(weather.get_weather_conditions(), 2) and \
-            self.assertEquals(weather.get_temperature_in_celsius(), 2.0) and \
-            self.assertEquals(weather.get_particulate_matter(), 51)
+        self.assertEquals(weather, [0, 1, 0, 0, 0, 0, 0, 2.0, 51])
 
     def test_between_files(self):
         weather_lookup = WeatherLookup("test")
         weather = weather_lookup.get_weather_snapshot("2016-01-23 01:15:15")
-        self.assertEquals(weather.get_weather_conditions(), 6) and \
-            self.assertEquals(weather.get_temperature_in_celsius(), 1.0) and \
-            self.assertEquals(weather.get_particulate_matter(), 98)
+        self.assertEquals(weather, [0, 0, 0, 0, 1, 0, 0, 1.0, 98])
 
     def test_exact_match(self):
         weather_lookup = WeatherLookup("test")
         weather = weather_lookup.get_weather_snapshot("2016-01-28 15:21:37")
-        self.assertEquals(weather.get_weather_conditions(), 4) and \
-            self.assertEquals(weather.get_temperature_in_celsius(), 5.0) and \
-            self.assertEquals(weather.get_particulate_matter(), 79)
+        self.assertEquals(weather, [0, 0, 0, 1, 0, 0, 0, 5.0, 79])
 
     def test_end_of_range(self):
         weather_lookup = WeatherLookup("test")
         weather = weather_lookup.get_weather_snapshot("2016-01-30 23:20:30")
-        self.assertEquals(weather.get_weather_conditions(), 3) and \
-            self.assertEquals(weather.get_temperature_in_celsius(), 3.0) and \
-            self.assertEquals(weather.get_particulate_matter(), 68)
+        self.assertEquals(weather, [0, 0, 1, 0, 0, 0, 0, 3.0, 68])
 
     def test_after_range(self):
         weather_lookup = WeatherLookup("test")
         weather = weather_lookup.get_weather_snapshot("2016-01-31 12:35:23")
-        self.assertEquals(weather.get_weather_conditions(), 3) and \
-            self.assertEquals(weather.get_temperature_in_celsius(), 3.0) and \
-            self.assertEquals(weather.get_particulate_matter(), 68)
+        self.assertEquals(weather, [0, 0, 1, 0, 0, 0, 0, 3.0, 68])
 
 """
 Self test
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from src.POI.POI import ReadPOI
 weather_lookup = WeatherLookup.WeatherLookup("test")
 print(weather_lookup.get_weather_snapshot("2016-01-30 23:22:30"))
 
-# poi = ReadPOI('data/season_1/test_set_1/poi_data/poi_data').readFile()
-# for x,v in poi.items():
-#     print (x,v)
+poi = ReadPOI('data/season_1/test_set_1/poi_data/poi_data').readFile()
+for x,v in poi.items():
+    print (x,v)
