df595e881c0b41a7120bd22dc29d15092a24cd22
Kimberly Williamson <kwilliamson@uen.org>
2017-03-05 12:51:20 -0700

    Re-basing.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
 import WeatherLookup
-=======
 #import src.Weather.WeatherLookup as WeatherLookup
->>>>>>> Stashed changes
 from src.POI.POI import ReadPOI
 '''
 weather_lookup = WeatherLookup.WeatherLookup("test")
@@ -14,6 +11,13 @@ poi = ReadPOI('data/season_1/test_set_1/poi_data/poi_data').readFile()
 print(weather_lookup.get_weather_snapshot("2016-01-24 15:21:53"))
 '''
 poi = ReadPOI('poi_data').readFile()
->>>>>>> Stashed changes
+
+#import src.Weather.WeatherLookup as WeatherLookup
+from src.POI.POI import ReadPOI
+'''
+weather_lookup = WeatherLookup.WeatherLookup("test")
+print(weather_lookup.get_weather_snapshot("2016-01-24 15:21:53"))
+'''
+poi = ReadPOI('poi_data').readFile()
 for x,v in poi.items():
     print (x,v)
