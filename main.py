ace427c650824b909328b12544feed880a215f7a
Kimberly Williamson <kwilliamson@uen.org>
2017-03-05 12:48:54 -0700

    Trying to merge commit.  So I can commit my new changes.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+<<<<<<< Updated upstream
 import WeatherLookup
+=======
+#import src.Weather.WeatherLookup as WeatherLookup
+>>>>>>> Stashed changes
 from src.POI.POI import ReadPOI
-
+'''
 weather_lookup = WeatherLookup.WeatherLookup("test")
+<<<<<<< Updated upstream
 print(weather_lookup.get_weather_snapshot("2016-01-30 23:22:30"))
 
 poi = ReadPOI('data/season_1/test_set_1/poi_data/poi_data').readFile()
+=======
+print(weather_lookup.get_weather_snapshot("2016-01-24 15:21:53"))
+'''
+poi = ReadPOI('poi_data').readFile()
+>>>>>>> Stashed changes
 for x,v in poi.items():
     print (x,v)
--- src/POI/POI.py
+++ src/POI/POI.py
@@ -4,32 +4,36 @@ Created on Feb 16, 2017
 @author: Kimberly Williamson
 '''
 
+def loopPOIClasses(pClass):
+    f = pClass.split(':')
+    fac = f[1].strip()
+    if '#' in f[0]:
+        first = f[0].split('#')[0]
+        second = f[0].split('#')[1]
+        entry = POIClass(fac, first, second)
+    else:
+        first = f[0]
+        entry = POIClass(fac, first)
+    
+    return entry
+
 class POIDivided:
     '''
     Store POIDivided data
     '''
-    def __init__(self, districtHash, poiClass):
-        self.districtHash = districtHash
+    def __init__(self, poiClass):
         self.poiClass = poiClass
-        
+
     def separatePOI(self):
-        #Create empty list to store individual POI classes
-        poiList = []
         poiSplit = self.poiClass.split('\t')
-        #loop through each pClass to separate out the facilities from the class levels.
+        classList = []
         for pClass in poiSplit:
-            f = pClass.split(':')
-            fac = f[1].strip()
-            if '#' in f[0]:
-                first = f[0].split('#')[0]
-                second = f[0].split('#')[1]
-                entry = POIClass(fac, first, second)
-            else:
-                first = f[0]
-                entry =POIClass(fac, first)
-            poiList.append(entry.createList())
+            #loop through each pClass to separate out the facilities from the class levels.
+            entry = loopPOIClasses(pClass)
+            classList.append(entry.createCatagories())
         #Return a tuple of the hash with a list of the POI classes
-        return self.districtHash, poiList
+        return classList
+        
         
 class POIClass:
     '''
@@ -40,9 +44,33 @@ class POIClass:
         self.firstLevel = firstLevel
         self.secondLevel = secondLevel
     
-    def createList(self):
-        return [self.facilities,self.firstLevel,self.secondLevel]
+    def getNumberFacilities(self):
+        return self.facilities
+    
+    def createCatagories(self):
+        if self.secondLevel is None:
+            self.secondLevel = ''
+        return self.firstLevel +'#'+ self.secondLevel
+    
+class POICatagoryAssignment:
+    def __init__(self, poiCatagories, districtHash,poiClass, districtList):
+        self.poiCatagories = poiCatagories
+        self.districtHash = districtHash
+        self.poiClass = poiClass
+        self.districtList = districtList
         
+    def createPOIDict(self):
+        poiSplit = self.poiClass.split('\t')
+        for pClass in poiSplit:
+            entry = loopPOIClasses(pClass)
+            poiCategory = entry.createCatagories()
+            idx = self.poiCatagories.index(poiCategory)
+            #need to find out how to replace a value
+            del self.districtList[idx]
+            self.districtList.insert(idx, entry.getNumberFacilities())
+        return self.districtHash, self.districtList
+        
+            
 class ReadPOI:
     '''
     Read the POI from file
@@ -52,14 +80,28 @@ class ReadPOI:
         
     def readFile(self):
         poiDict = {}
+        poiCategories = set([])
+        poiListCategories = []
         #Read the POI file
         with open(self.file, 'r') as poiFile:
             #Loop through the each line and separate the district hash from the POI class.  Then add POIDivided results to a dictionary.
             for row in poiFile:
                 rowSplit = row.split('\t',1)
-                x,v = POIDivided(rowSplit[0],rowSplit[1]).separatePOI()
+                poiList = POIDivided(rowSplit[1]).separatePOI()
+                for item in poiList:
+                    poiCategories.add(item)
+            poiListCategories = list(poiCategories)
+            totalCatagories = len(poiListCategories)
+        with open(self.file, 'r') as poiFile:
+            for row in poiFile:
+                districtList = []
+                for z in range(totalCatagories):
+                    districtList.append(0)
+                rowSplit = row.split('\t',1)
+                x,v = POICatagoryAssignment(poiListCategories, rowSplit[0], rowSplit[1], districtList).createPOIDict()
                 poiDict[x] = v
         return poiDict
+
             
 
 
